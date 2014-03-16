129b2abd438fd4e71a975ab5757f7d6c5f6b6522
Maggie Shine <magksh@gmail.com>
2014-03-15 22:41:43 -0700

    Beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gifdump::Application.config.secret_key_base = '19f4f7b42380a78b125feeb77e92cc17b4451556d26860fa2bf48c1ce969bbf0afc70b777a2d8e31dea747ea0047d1dd6dad74e3cad68eed2e50dd1bb85482a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gifdump::Application.config.session_store :cookie_store, key: '_gifdump_session'
