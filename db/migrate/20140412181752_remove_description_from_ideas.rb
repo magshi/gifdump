00464d229e7f5c18b8003985fd5dbb694b583df8
Maggie Shine <magksh@gmail.com>
2014-04-12 11:49:43 -0700

    Got rid of the bad migration files

--- db/migrate/20140412181752_remove_description_from_ideas.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescriptionFromIdeas < ActiveRecord::Migration
-  def change
-  	remove_column :ideas, :description
-  end
-end
--- db/migrate/20140412181944_remove_desc_from_topic.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDescFromTopic < ActiveRecord::Migration
-  def change
-    remove_column :topics, :description, :text
-  end
-end
